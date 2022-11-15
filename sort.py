62fe1e4a8f45094277b9dae6d511e0709f1ed437
shihab81 <74010054+shihab81@users.noreply.github.com>
2022-11-15 08:48:35 +0600

    Selection sort.py
    updated by selection sort

--- sort.py
+++ sort.py
@@ -1,1 +1,16 @@
-print("Hello")
+def sort(nums):
+  
+  for i in range(5):
+    
+    minpos = i
+    for j in range(i,6):
+      if nums[j]<nums[minpos]:
+        minpos=j
+        
+    temp = nums[i]
+    nums[i] = nums[minpos]
+    nums[minpos]=temp
+    
+nums = [5,3,8,6,7,2]
+sort(nums)
+point(nums)
